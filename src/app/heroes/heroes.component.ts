3ca4aeeba6099246692fd809a75a84e1657c62a3
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2021-01-10 22:31:05 -0700

    dashboard set

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,23 +13,15 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes?:Hero[];
-  selectedHero?: Hero;
 
-  constructor(private heroService:HeroService, private messageService:MessageService) { }
+  constructor(private heroService:HeroService) { }
 
   ngOnInit(): void {
     this.getHeros();
   }
 
   getHeros():void{
-    this.heroService.getHeros()
+    this.heroService.getHeroes()
       .subscribe(heroes=>  this.heroes = heroes);
   }
-
-  onSelect(hero:Hero):void{
-    this.selectedHero=hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-
-  }
-
 }
